9c73ea3df97907abbe2ead3d2e3d724b297f6f9a
Nagawade <maulinagawade5@gmail.com>
2025-08-01 18:23:38 +0530

    Second Commit

---
+++ DemoFirst/src/com/demo/Second.java
@@ -0,0 +1,36 @@
+package com.demo;
+import java.util.*;
+
+public class Second {
+	public static void main(String[] args) {
+		
+		int value = 9/4;
+		System.out.println(value);
+		
+		float value2 = 100f/3f;
+		System.out.println(value2);
+		
+		double value3 = 100d/3d;
+		System.out.println(value3);
+		
+		int r = 10;
+		double area = 3.14 * r *r;
+		System.out.println("Area of circle :"+area);
+		
+		char ch = 65;
+		System.out.println(ch); // returns the ascii value of the number
+		
+		short shortMin = 32767;
+		short shortMax = -32768;
+		
+		System.out.println("Minimum range of short :"+shortMin);
+		System.out.println("Maximum range of short :"+shortMax);
+		
+		
+		
+		
+		
+		
+	}
+
+}
